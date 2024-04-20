994fc3e6bf81ed60a2a4f330ae63899ae2d788e4
RobsNotHere <166869306+RobsNotHere@users.noreply.github.com>
2024-04-19 21:47:48 -0700

    Add files via upload

---
+++ Landing Pages/src/components/assets/PexelsPixabay4610641.jsx
@@ -0,0 +1,30 @@
+export default function PexelsPixabay4610641({ className = "" }) {
+  return (
+    <div
+      className={`font-inter bg-pexels-pixabay inline-flex w-full flex-col items-start rounded-[40px] bg-cover bg-center text-center leading-[normal] tracking-[0px] text-white ${className}`}
+    >
+      <div className="flex flex-grow flex-col items-center justify-end gap-y-6 self-stretch rounded-[40px] bg-black/60 pb-36 pl-14 pr-16 pt-44" >
+        <div className="flex items-center justify-center self-stretch text-base font-normal leading-[normal]" >
+          <p>
+            Find a project that perfectly matches your specialization. No more applying for irrelevant jobs.
+          </p>
+        </div>
+        <div className="flex items-center justify-center self-stretch text-2xl font-bold leading-[normal]" >
+          <p>Work Opportunities</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * This component was generated from Figma with FireJet.
+ * Learn more at https://www.firejet.io
+ *
+ * README:
+ * The output code may look slightly different when copied to your codebase. To fix this:
+ * 1. Include the necessary fonts. The required fonts are imported from public/index.html
+ * 2. Include the global styles. They can be found in App.css
+ *
+ * Note: Step 2 is not required for tailwind.css output
+ */
