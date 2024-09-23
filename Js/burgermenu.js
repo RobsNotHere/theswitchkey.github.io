785cf0021ca60bfeab7cca79b9daa3b06c4b5ce4
RobsNotHere <166869306+RobsNotHere@users.noreply.github.com>
2024-09-22 20:06:13 -0700

    y

--- Js/burgermenu.js
+++ Js/burgermenu.js
@@ -4,7 +4,11 @@ function myburgerLink() {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+  } 
+  var y = document.getElementById("logo-icon");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
   }
-
-  
 }
