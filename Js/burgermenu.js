ce3058e1d81181fe56a6c4e9163b6e958d28181d
RobsNotHere <166869306+RobsNotHere@users.noreply.github.com>
2024-09-22 23:34:31 -0700

    u

--- Js/burgermenu.js
+++ Js/burgermenu.js
@@ -1,10 +1,8 @@
 function myburgerLink() {
   var x = document.getElementById("hamburger-page-nav");
   if (x.style.display === "block") {
-    document.getElementById("logo-icon")
     x.style.display = "none";
   } else {
-    document.getElementById("logo-icon")
     x.style.display = "block";
   } 
 }
