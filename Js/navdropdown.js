dc16b32403cc1378c7ae81ee11f7731c7698ec2d
RobsNotHere <166869306+RobsNotHere@users.noreply.github.com>
2024-12-22 17:29:05 -0800

    u

--- Js/navdropdown.js
+++ Js/navdropdown.js
@@ -1,11 +1,8 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function myDropdown() {
+  var x = document.getElementById("sub-menu-content");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-    var x = document.getElementById("sub-menu-content");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    } 
-  
-  
+}
